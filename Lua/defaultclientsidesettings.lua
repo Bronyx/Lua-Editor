c0a953ddf27e7bd238d09b9331ec026ebec1793d
ricky122122 <111702225+ricky122122@users.noreply.github.com>
2024-05-23 13:18:10 -0500

    Delete Lua directory

--- Lua/defaultclientsidesettings.lua
+++
@@ -1,9 +0,0 @@
--- DO NOT EDIT THESE SETTINGS, THIS IS JUST A TEMPLATE --
-
-local clientsidesettings = {}
-
-clientsidesettings.targetnoninteractable = "False"
-
-clientsidesettings.targetitems = false
-
-return clientsidesettings
